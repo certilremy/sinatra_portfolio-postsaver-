f607109cb559d280034299abe140b0afc1a512b1
certilremy <certilremy@gmail.com>
2019-03-01 13:07:44 -0500

    users can login now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UserController < ApplicationController
         # binding.pry
         @user = User.find_by(email: params["email"],password: params["password"])
         session[:user_id] = @user.id
+        @post = Post.all
         erb :"/post/index"
     
       end
