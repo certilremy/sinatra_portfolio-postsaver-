4328ae3e90f82dbd3e5f97052199599d57cf18f9
certilremy <certilremy@gmail.com>
2019-03-01 13:00:05 -0500

    user can signup now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,38 +2,42 @@ class UserController < ApplicationController
     
       get 'users/show' do 
         #@user = User.find(params[:id])
-        erb :'users/show'
+        erb :'/users/show'
       end
 
       post '/users/login' do
         # binding.pry
-        @user = User.find_by(email: params["email"],pasword: params["password"])
+        @user = User.find_by(email: params["email"],password: params["password"])
         session[:user_id] = @user.id
         erb :"/post/index"
     
       end
 
-      get 'users/login' do 
+      get '/users/login' do 
         
-        erb :'users/login'
+        erb :'/users/login'
       end
+
+      get '/users/signup' do 
+        
+        erb :'/users/signup'
+      end
+
 
       post "/users/signup" do
         # raise params.inspect
         name = params[:name]
         email = params[:email]
         pasword = params[:password]
-        @user = User.create(name:name,email:email,pasword:pasword )
+        @user = User.create(name:name,email:email,password:pasword )
         session[:user_id] = @user.id
+
+      @post = Post.all
         erb :"/post/index"
         
       end
 
-      get 'users/signup' do 
-        
-        erb :'users/signup'
-      end
-
+   
     
       get "/users/:id" do
        # @user = User.find(params[:id])
