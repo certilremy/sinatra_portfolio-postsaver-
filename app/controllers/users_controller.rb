5b6f50821e822a3b3a5927d4c9f536b0eff18a64
certilremy <certilremy@gmail.com>
2019-03-01 12:03:59 -0500

    added the relation between post and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UserController < ApplicationController
     
+      get 'users/show' do 
+        #@user = User.find(params[:id])
+        erb :'users/show'
+      end
+
+      get 'users/login' do 
+        
+        erb :'users/login'
+      end
+
+      get 'users/signup' do 
+        
+        erb :'users/signup'
+      end
+
+    
+      get "/users/:id" do
+       # @user = User.find(params[:id])
+        erb :"/users/show"
+      end
+          
+    
 end
