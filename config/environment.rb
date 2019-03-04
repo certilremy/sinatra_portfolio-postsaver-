6eb9e80f7886fe2d3432873fa4d94bd975d8887b
certilremy <certilremy@gmail.com>
2019-03-04 15:43:31 -0500

    added the posgress database

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
+
+
+
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
@@ -10,3 +13,17 @@ ActiveRecord::Base.establish_connection(
 
 require './app/controllers/application_controller'
 require_all 'app'
+
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+    )
+end
